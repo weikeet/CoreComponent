a19cab229b77f5635eb4684e9df4e02f1ce35fb1
zhengwei.zhang <zhengwei.zhang@enerjoy.life>
2021-06-15 21:00:15 +0800

    add ProcessUtils

---
+++ utils/src/main/java/com/weicools/utils/process/ProcessUtils.kt
@@ -0,0 +1,41 @@
+package com.weicools.utils.process
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.os.Build
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
+
+/**
+ * @author weicools
+ * @date 2021.06.15
+ *
+ * android获取进程名函数
+ * https://juejin.cn/post/6877127949452050446
+ */
+object ProcessUtils {
+  /** Get name of current process. */
+  fun getProcessName(): String? {
+    return if (Build.VERSION.SDK_INT >= 28)
+      Application.getProcessName()
+    else try {
+      // Using the same technique as Application.getProcessName() for older devices
+      // Using reflection since ActivityThread is an internal API
+      @SuppressLint("PrivateApi")
+      val activityThread = Class.forName("android.app.ActivityThread")
+      // Before API 18, the method was incorrectly named "currentPackageName", but it still returned the process name
+      // See https://github.com/aosp-mirror/platform_frameworks_base/commit/b57a50bd16ce25db441da5c1b63d48721bb90687
+      val methodName = if (Build.VERSION.SDK_INT >= 18) "currentProcessName" else "currentPackageName"
+      val getProcessName: Method = activityThread.getDeclaredMethod(methodName)
+      getProcessName.invoke(null) as String
+    } catch (e: ClassNotFoundException) {
+      throw RuntimeException(e)
+    } catch (e: NoSuchMethodException) {
+      throw RuntimeException(e)
+    } catch (e: IllegalAccessException) {
+      throw RuntimeException(e)
+    } catch (e: InvocationTargetException) {
+      throw RuntimeException(e)
+    }
+  }
+}
