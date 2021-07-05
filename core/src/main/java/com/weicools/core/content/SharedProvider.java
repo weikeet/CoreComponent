f965b7bb315fc2170b2ec5f57c5bd3e986c52ee2
zhengwei.zhang <zhengwei.zhang@enerjoy.life>
2021-07-05 15:37:36 +0800

    add SharedProvider

---
+++ core/src/main/java/com/weicools/core/content/SharedProvider.java
@@ -0,0 +1,42 @@
+package com.weicools.core.content;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * @author weicools
+ * @date 2021.05.30
+ */
+public abstract class SharedProvider extends ContentProvider {
+  @Nullable
+  @Override
+  public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public String getType(@NonNull Uri uri) {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+    return null;
+  }
+
+  @Override
+  public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+    return 0;
+  }
+
+  @Override
+  public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+    return 0;
+  }
+}
