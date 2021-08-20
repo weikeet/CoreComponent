173ee10ffce0cb61f332db51c0af9dc208135804
zhengwei.zhang <zhengwei.zhang@enerjoy.life>
2021-08-20 11:26:20 +0800

    优化 getProcessName

--- utils/src/main/java/com/weicools/utils/process/ProcessUtils.kt
+++ utils/src/main/java/com/weicools/utils/process/ProcessUtils.kt
@@ -3,39 +3,87 @@ package com.weicools.utils.process
 import android.annotation.SuppressLint
 import android.app.Application
 import android.os.Build
-import java.lang.reflect.InvocationTargetException
+import android.os.Process
+import android.text.TextUtils
+import android.util.Log
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
 import java.lang.reflect.Method
 
 /**
  * @author weicools
  * @date 2021.06.15
  *
- * android获取进程名函数
- * https://juejin.cn/post/6877127949452050446
+ * References:
+ * - https://juejin.cn/post/6877127949452050446
+ * - https://developer.android.google.cn/reference/android/app/Application#getProcessName()
  */
 object ProcessUtils {
-  /** Get name of current process. */
-  fun getProcessName(): String? {
-    return if (Build.VERSION.SDK_INT >= 28)
-      Application.getProcessName()
-    else try {
-      // Using the same technique as Application.getProcessName() for older devices
-      // Using reflection since ActivityThread is an internal API
+  private const val TAG = "ProcessUtils"
+
+  private var processName: String? = null
+
+  @JvmStatic
+  fun getProcessName(): String {
+    val name = processName
+    if (name != null && TextUtils.isEmpty(name)) {
+      return name
+    }
+
+    if (Build.VERSION.SDK_INT >= 28) {
+      val appProcessName = Application.getProcessName()
+      Log.d(TAG, "getProcessName: by Application=$appProcessName")
+      processName = appProcessName
+      return appProcessName
+    }
+
+    val reflectProcessName = getProcessNameByReflect()
+    if (reflectProcessName != null && TextUtils.isEmpty(reflectProcessName)) {
+      Log.d(TAG, "getProcessName: by Reflect=$reflectProcessName")
+      processName = reflectProcessName
+      return reflectProcessName
+    }
+
+    val cmdlineProcessName = getProcessNameByCmdline()
+    if (cmdlineProcessName != null && TextUtils.isEmpty(cmdlineProcessName)) {
+      Log.d(TAG, "getProcessName: by Cmdline=$cmdlineProcessName")
+      processName = cmdlineProcessName
+      return cmdlineProcessName
+    }
+
+    return "";
+  }
+
+  private fun getProcessNameByReflect(): String? {
+    if (Build.VERSION.SDK_INT < 18) {
+      return null
+    }
+
+    var processName: String? = null
+    try {
       @SuppressLint("PrivateApi")
       val activityThread = Class.forName("android.app.ActivityThread")
-      // Before API 18, the method was incorrectly named "currentPackageName", but it still returned the process name
-      // See https://github.com/aosp-mirror/platform_frameworks_base/commit/b57a50bd16ce25db441da5c1b63d48721bb90687
-      val methodName = if (Build.VERSION.SDK_INT >= 18) "currentProcessName" else "currentPackageName"
-      val getProcessName: Method = activityThread.getDeclaredMethod(methodName)
-      getProcessName.invoke(null) as String
-    } catch (e: ClassNotFoundException) {
-      throw RuntimeException(e)
-    } catch (e: NoSuchMethodException) {
-      throw RuntimeException(e)
-    } catch (e: IllegalAccessException) {
-      throw RuntimeException(e)
-    } catch (e: InvocationTargetException) {
-      throw RuntimeException(e)
+
+      @SuppressLint("PrivateApi")
+      val getProcessName: Method = activityThread.getDeclaredMethod("currentProcessName")
+
+      processName = getProcessName.invoke(null) as String?
+    } catch (e: Throwable) {
     }
+    return processName
+  }
+
+  private fun getProcessNameByCmdline(): String? {
+    var processName: String? = null
+    try {
+      val file = File("/proc/" + Process.myPid() + "/" + "cmdline")
+      val bufferedReader = BufferedReader(FileReader(file))
+      processName = bufferedReader.readLine().trim { it <= ' ' }
+      bufferedReader.close()
+    } catch (e: Throwable) {
+      e.printStackTrace()
+    }
+    return processName
   }
 }
