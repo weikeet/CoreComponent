cbaf0727942ae6eb7cf288d72b82d802314d6541
lecymeng <1570682285@qq.com>
2021-09-21 20:47:58 +0800

    test multi process

--- app/src/main/java/com/weicools/component/sample/SampleApp.kt
+++ app/src/main/java/com/weicools/component/sample/SampleApp.kt
@@ -1,6 +1,11 @@
 package com.weicools.component.sample
 
 import android.app.Application
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import com.weicools.component.sample.preferences.PreferencesProvider
+import com.weicools.utils.process.ProcessUtils
 
 /**
  * @author Weicools
@@ -8,4 +13,25 @@ import android.app.Application
  * @date 2021.07.04
  */
 class SampleApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (mainProcess()) {
+            Log.d("SampleApp", "onCreate: mainProcess")
+            contentResolver.call(PreferencesProvider.getPreferenceUri(), "GET_NAME", null, Bundle())
+        } else if (workProcess()) {
+            Log.d("SampleApp", "onCreate: workProcess")
+        }
+    }
+
+    private fun mainProcess(): Boolean {
+        val name = ProcessUtils.getProcessName()
+        return packageName.equals(name) || TextUtils.isEmpty(name)
+    }
+
+    private fun workProcess(): Boolean {
+        val name = ProcessUtils.getProcessName()
+        return name.endsWith("work")
+    }
 }
