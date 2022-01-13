ebb523350ce906813254fa591113ba4d35b2a44c
Weiwei <1570682285@qq.com>
2022-01-13 14:46:50 +0800

    set return type, add dimen res

--- core-ktx/src/main/java/com/weiwei/extensions/Resources.kt
+++ core-ktx/src/main/java/com/weiwei/extensions/Resources.kt
@@ -4,6 +4,7 @@ import android.content.res.ColorStateList
 import android.content.res.Resources
 import android.graphics.drawable.Drawable
 import androidx.annotation.ColorRes
+import androidx.annotation.DimenRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.appcompat.content.res.AppCompatResources
@@ -19,11 +20,17 @@ val widthPixels get() = (Resources.getSystem().displayMetrics.widthPixels)
 
 val heightPixels get() = (Resources.getSystem().displayMetrics.heightPixels)
 
-fun stringOf(@StringRes stringId: Int): String = LibGlobalScope.context.getString(stringId)
+fun stringOf(@StringRes stringId: Int): String =
+  LibGlobalScope.context.getString(stringId)
 
-fun colorOf(@ColorRes colorId: Int): Int = ContextCompat.getColor(LibGlobalScope.context, colorId)
+fun stringsOf(@StringRes stringId: Int, vararg formatArgs: Any) =
+  LibGlobalScope.context.getString(stringId, *formatArgs)
 
-fun colorsOf(@ColorRes colorId: Int): ColorStateList? = ContextCompat.getColorStateList(LibGlobalScope.context, colorId)
+fun colorOf(@ColorRes colorId: Int): Int =
+  ContextCompat.getColor(LibGlobalScope.context, colorId)
+
+fun colorsOf(@ColorRes colorId: Int): ColorStateList? =
+  ContextCompat.getColorStateList(LibGlobalScope.context, colorId)
 
 fun drawableOf(@DrawableRes drawableId: Int): Drawable? =
   if (atLeastL()) {
@@ -31,3 +38,12 @@ fun drawableOf(@DrawableRes drawableId: Int): Drawable? =
   } else {
     AppCompatResources.getDrawable(LibGlobalScope.context, drawableId)
   }
+
+fun dimenOf(@DimenRes dimenId: Int): Float =
+  LibGlobalScope.context.resources.getDimension(dimenId)
+
+fun dimenSizeOf(@DimenRes dimenId: Int): Int =
+  LibGlobalScope.context.resources.getDimensionPixelSize(dimenId)
+
+fun dimenOffsetOf(@DimenRes dimenId: Int): Int =
+  LibGlobalScope.context.resources.getDimensionPixelOffset(dimenId)
